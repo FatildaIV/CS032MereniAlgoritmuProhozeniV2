472b931d34e745e7613749bc089968176200f314
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 13:07:17 +0200

    Initial commit

---
+++ CS032MereniAlgoritmuProhozeni/CS030PresneMereniCasu/MereniAlgoritmuProhozeniForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace CS031MereniAlgoritmuProhozeni
+{
+    partial class MereniAlgoritmuProhozeniForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testButton = new System.Windows.Forms.Button();
+            this.vystupTextBox = new System.Windows.Forms.TextBox();
+            this.meritProhozeniButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // testButton
+            // 
+            this.testButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.testButton.Location = new System.Drawing.Point(97, 71);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(241, 107);
+            this.testButton.TabIndex = 0;
+            this.testButton.Text = "Spustit Test";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.Click += new System.EventHandler(this.testButton_Click);
+            // 
+            // vystupTextBox
+            // 
+            this.vystupTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.vystupTextBox.Location = new System.Drawing.Point(12, 222);
+            this.vystupTextBox.Multiline = true;
+            this.vystupTextBox.Name = "vystupTextBox";
+            this.vystupTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.vystupTextBox.Size = new System.Drawing.Size(776, 216);
+            this.vystupTextBox.TabIndex = 1;
+            // 
+            // meritProhozeniButton
+            // 
+            this.meritProhozeniButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.meritProhozeniButton.Location = new System.Drawing.Point(458, 71);
+            this.meritProhozeniButton.Name = "meritProhozeniButton";
+            this.meritProhozeniButton.Size = new System.Drawing.Size(241, 107);
+            this.meritProhozeniButton.TabIndex = 2;
+            this.meritProhozeniButton.Text = "Měřit prohození";
+            this.meritProhozeniButton.UseVisualStyleBackColor = true;
+            this.meritProhozeniButton.Click += new System.EventHandler(this.meritProhozeniButton_Click);
+            // 
+            // MereniAlgoritmuProhozeniForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.meritProhozeniButton);
+            this.Controls.Add(this.vystupTextBox);
+            this.Controls.Add(this.testButton);
+            this.Name = "MereniAlgoritmuProhozeniForm";
+            this.Text = "CS031 Meření Algoritmu Prohození - Vojtěch Miler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button testButton;
+        private System.Windows.Forms.TextBox vystupTextBox;
+        private System.Windows.Forms.Button meritProhozeniButton;
+    }
+}
+
---
+++ CS032MereniAlgoritmuProhozeni/CS030PresneMereniCasu/MereniAlgoritmuProhozeniForm.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace CS031MereniAlgoritmuProhozeni
+{
+    public partial class MereniAlgoritmuProhozeniForm : Form
+    {
+        public MereniAlgoritmuProhozeniForm()
+        {
+            InitializeComponent();
+        }
+
+        private void testButton_Click(object sender, EventArgs e)
+        {
+            StopkyTestDateTime();
+            vystupTextBox.AppendText(Environment.NewLine);
+            vystupTextBox.AppendText(Environment.NewLine);
+            StopkyTestHighResolutionDateTime();
+            vystupTextBox.AppendText(Environment.NewLine);
+            vystupTextBox.AppendText(Environment.NewLine);
+            StopkyTestStopwatchHashSet();
+            vystupTextBox.AppendText(Environment.NewLine);
+            vystupTextBox.AppendText(Environment.NewLine);
+            StopkyTestStopwatch();
+            vystupTextBox.AppendText(Environment.NewLine);
+            vystupTextBox.AppendText(Environment.NewLine);
+            StopkyTestStopwatchDrift();
+            vystupTextBox.AppendText(Environment.NewLine);
+            vystupTextBox.AppendText(Environment.NewLine);
+        }
+
+        private void StopkyTestDateTime() {
+
+            vystupTextBox.AppendText("Testing DateTime for 1 seconds...");
+
+            var distinctValues = new HashSet<DateTime>();
+            var sw = Stopwatch.StartNew();
+
+            while (sw.Elapsed.TotalSeconds < 1)
+            {
+                distinctValues.Add(DateTime.UtcNow);
+            }
+
+            sw.Stop();
+
+            vystupTextBox.AppendText(Environment.NewLine);
+            vystupTextBox.AppendText(
+                string.Format(
+                    "Precision: {0:0.000000} ms ({1} samples)",
+                    sw.Elapsed.TotalMilliseconds / distinctValues.Count,
+                    distinctValues.Count));
+
+        }
+
+        private void StopkyTestHighResolutionDateTime()
+        {
+
+            vystupTextBox.AppendText("Testing High Resolution DateTime for 1 seconds...");
+
+            var distinctValues = new HashSet<DateTime>();
+            var sw = Stopwatch.StartNew();
+
+            while (sw.Elapsed.TotalSeconds < 1)
+            {
+                distinctValues.Add(HighResolutionDateTime.UtcNow);
+            }
+
+            sw.Stop();
+
+            vystupTextBox.AppendText(Environment.NewLine);
+            vystupTextBox.AppendText(
+                string.Format(
+                    "Precision: {0:0.000000} ms ({1} samples)",
+                    sw.Elapsed.TotalMilliseconds / distinctValues.Count,
+                    distinctValues.Count));
+
+        }
+
+        private void StopkyTestStopwatchHashSet()
+        {
+
+            vystupTextBox.AppendText("Testing Stopwatch for 1 seconds...");
+
+            var distinctValues = new HashSet<long>();
+            var sw = Stopwatch.StartNew();
+
+            while (sw.Elapsed.TotalSeconds < 1)
+            {
+                distinctValues.Add(sw.ElapsedTicks);
+            }
+
+            sw.Stop();
+
+            vystupTextBox.AppendText(Environment.NewLine);
+            vystupTextBox.AppendText(
+                string.Format(
+                    "Resolution: {0:0.000000} ms ({1} samples)",
+                    sw.Elapsed.TotalMilliseconds / distinctValues.Count,
+                    distinctValues.Count));
+
+        }
+
+        private void StopkyTestStopwatch()
+        {
+
+            vystupTextBox.AppendText("Testing Stopwatch for 1 seconds...");
+
+            var sw = Stopwatch.StartNew();
+
+            while (sw.Elapsed.TotalSeconds < 1)
+            {
+                
+            }
+
+            sw.Stop();
+
+            vystupTextBox.AppendText(Environment.NewLine);
+            vystupTextBox.AppendText(
+                string.Format(
+                    "Resolution: {0:0.000000} ms ({1} samples)",
+                    sw.Elapsed.TotalMilliseconds / sw.ElapsedTicks,
+                    sw.ElapsedTicks));
+
+        }
+
+        private void StopkyTestStopwatchDrift() {
+            var start = HighResolutionDateTime.UtcNow;
+            var sw = Stopwatch.StartNew();
+
+            while (sw.Elapsed.TotalSeconds < 10)
+            {
+                DateTime nowBasedOnStopwatch = start + sw.Elapsed;
+                TimeSpan diff = HighResolutionDateTime.UtcNow - nowBasedOnStopwatch;
+
+                vystupTextBox.AppendText(string.Format("Stopwatch to UTC drift: {0:0.000000} ms", diff.TotalMilliseconds));
+                vystupTextBox.AppendText(Environment.NewLine);
+
+                Thread.Sleep(1000);
+            }
+        }
+        private void ProhoditPromenna<T>(ref T a, ref T b)
+        {
+            T pom = a;
+            a = b;
+            b = pom;
+        }
+        private void ProhoditPromenna(ref int a, ref int b)
+        {
+            int pom = a;
+            a = b;
+            b = pom; 
+        }
+        public delegate void ProceduraProhozeni(ref int a, ref int b);
+
+        private long OpakovatProhozeni(ProceduraProhozeni proceduraProhozeni, int n)
+        {
+            var sw = Stopwatch.StartNew();
+            int a = 1;
+            int b = 2;
+            for (int i = 0; i< n; i++)
+            {
+                proceduraProhozeni(ref a, ref b);
+            }
+            return sw.ElapsedMilliseconds;
+        }
+        private void MeritProhozeni(int max)
+        {
+            VytizitProcesor();
+            string vypis = "Prohození s pomocnou proměnnou {1}x: {0:0.000000} ms";
+            vypis = "{0};{1:0.000000};{2:0.000000}";
+            for (int i = 1; i < max; i *= 10)
+            {
+
+
+
+
+
+                vystupTextBox.AppendText(
+                    string.Format(
+                        vypis,
+                        i,
+                         OpakovatProhozeni(ProhoditPromenna, i),
+                         OpakovatProhozeni(ProhoditPromenna<int>, i)));
+              
+
+                vystupTextBox.AppendText(Environment.NewLine);
+
+            }
+        }
+
+        private void meritProhozeniButton_Click(object sender, EventArgs e)
+        {
+            MeritProhozeni(100000000);
+        }
+        private void VytizitProcesor()
+        {
+            OpakovatProhozeni(ProhoditPromenna,100000000);
+        }
+    }
+}
